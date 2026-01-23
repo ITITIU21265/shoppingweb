8eb160feca0ccdd2c301cd6a05c85a50b1bf2ae9
ITITIU21265 <lequangnguyen0912174992@gmail.com>
2026-01-23 21:38:54 +0700

    add controller

--- src/main/java/com/web/shoppingweb/controller/AuthController.java
+++ src/main/java/com/web/shoppingweb/controller/AuthController.java
@@ -1,35 +1,53 @@
 package com.web.shoppingweb.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.web.shoppingweb.dto.LoginRequestDTO;
 import com.web.shoppingweb.dto.LoginResponseDTO;
-import com.web.shoppingweb.service.CustomerService;
+import com.web.shoppingweb.service.UserService;
 
 import jakarta.validation.Valid;
 
 
 
-@RestController
-@RequestMapping("/api/auth")
+@Controller
+@RequestMapping
 @CrossOrigin(origins="*")
 public class AuthController {
 
     @Autowired
-    private CustomerService customerService;
+    private UserService userService;
 
-    @PostMapping("/login")
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @PostMapping("/api/auth/login")
+    @ResponseBody
     public ResponseEntity<LoginResponseDTO> login(@Valid @RequestBody LoginRequestDTO loginRequest) {
-        LoginResponseDTO response = customerService.login(loginRequest);
+        LoginResponseDTO response = userService.login(loginRequest);
         return ResponseEntity.ok(response);
     }
-    
 
-    
+    @PostMapping(path = "/api/auth/login", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @ResponseBody
+    public ResponseEntity<LoginResponseDTO> loginForm(
+            @RequestParam("username") String username,
+            @RequestParam("password") String password
+    ) {
+        LoginRequestDTO loginRequest = new LoginRequestDTO(username, password);
+        LoginResponseDTO response = userService.login(loginRequest);
+        return ResponseEntity.ok(response);
+    }
 }
