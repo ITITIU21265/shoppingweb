2d048955c3720410358ae0a693c4604f49a3c195
ITITIU21265 <lequangnguyen0912174992@gmail.com>
2026-01-25 22:37:10 +0700

    change to interger id to fit the data schema

--- src/main/java/com/web/shoppingweb/entity/Role.java
+++ src/main/java/com/web/shoppingweb/entity/Role.java
@@ -12,7 +12,7 @@ import jakarta.persistence.Table;
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     @Column(nullable = false, unique = true, length = 30)
     private String code;
@@ -20,11 +20,11 @@ public class Role {
     @Column(nullable = false, length = 100)
     private String name;
 
-    public Long getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
